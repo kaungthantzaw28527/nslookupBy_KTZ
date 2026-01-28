aa7031063ad29cebbe100e1dd674bb08729933a8
Kaung Thant Zaw <kaungthantzaw28527@gmail.com>
2026-01-28 11:35:35 +0630

    Initial commit: nslookup project started

---
+++ nslookup.py
@@ -0,0 +1,37 @@
+import dns.resolver
+import socket
+
+def nslookupbyKTZ():
+    print("="*30)
+    userinputdomain = input("Enter Domain : ").strip()
+    print("---- information ------")
+
+    if not userinputdomain:
+        print("Something is wrong or Error: Domain name cannot be empty.")
+        return
+    try:
+        ip_address = socket.gethostbyname(userinputdomain)
+        print(f"[*] IPv4 Address : {ip_address}")
+    except:
+        print("[!] Could not resolve IPv4 address.")
+
+    try:
+        mx_records = dns.resolver.resolve(userinputdomain, 'MX')
+        for mx in mx_records:
+            print(f"[*] Mail Server  : {mx.exchange} (Priority: {mx.preference})")
+    except:
+        print("[ ] No MX records found.")
+
+    try:
+        ns_records = dns.resolver.resolve(userinputdomain, 'NS')
+        for ns in ns_records:
+            print(f"[*] Name Server  : {ns.to_text()}")
+    except:
+        print("[ ] No NS records found.")
+    
+    print("="*30)
+
+if __name__ == "__main__":
+    nslookupbyKTZ()
+
+#owner = kaunglinux.dev@gmail.com,kaungthantzaw28527@gmail.com
